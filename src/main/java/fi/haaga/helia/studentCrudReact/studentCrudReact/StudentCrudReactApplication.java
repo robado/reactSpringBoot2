5ab8cb12f925c595dfbca5300cb0c86e2d3c5b2d
Robert Ridal <ridal.robert@gmail.com>
2018-04-25 19:55:18 +0300

    Added creation files

---
+++ src/main/java/fi/haaga/helia/studentCrudReact/studentCrudReact/StudentCrudReactApplication.java
@@ -0,0 +1,12 @@
+package fi.haaga.helia.studentCrudReact.studentCrudReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCrudReactApplication.class, args);
+	}
+}
